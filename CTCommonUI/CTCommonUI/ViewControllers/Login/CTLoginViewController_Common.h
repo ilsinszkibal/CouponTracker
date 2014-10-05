5a289c51068302c1c8d2273ba29736d6ece981b3
Teveli László <laszlo.teveli@distinction.hu>
2014-10-05 11:46:21 +0200

    Restructuring

---
+++ CTCommonUI/CTCommonUI/ViewControllers/Login/CTLoginViewController_Common.h
@@ -0,0 +1,13 @@
+//
+//  CTLoginViewController.h
+//  CTCommonUI
+//
+//  Created by Teveli László on 05/10/14.
+//  Copyright (c) 2014 Balazs Ilsinszki. All rights reserved.
+//
+
+#import "CTViewController.h"
+
+@interface CTLoginViewController_Common : CTViewController
+
+@end
