fe3e07fd7f8d34c60df9cf58300ff5ffadada8a9
ilsinszkibal <balazsilsinszki@Balazs-MacBook-Pro.local>
2014-10-24 10:03:25 +0200

    Passing view navigation animation fix
    Fixed the navigation of the passing animation. Subviews are animating
    now.

--- CTUtils/CTUtils/TransitioningDelegate/Transitions/PassingViewNavigation/CTPassingViewNavigationAnimatedTransitioning.m
+++ CTUtils/CTUtils/TransitioningDelegate/Transitions/PassingViewNavigation/CTPassingViewNavigationAnimatedTransitioning.m
@@ -99,7 +99,7 @@
                               delay:0
              usingSpringWithDamping:300.0
               initialSpringVelocity:5.0
-                            options:UIViewAnimationOptionAllowUserInteraction | UIViewAnimationOptionBeginFromCurrentState
+                            options:UIViewAnimationOptionAllowUserInteraction | UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionLayoutSubviews
                          animations:^{
                              [presentingView setFrame:presentingFinalRect];
                              [currentView setFrame:currentViewFinalRect];
@@ -166,7 +166,7 @@
                               delay:0
              usingSpringWithDamping:300.0
               initialSpringVelocity:5.0
-                            options:UIViewAnimationOptionAllowUserInteraction | UIViewAnimationOptionBeginFromCurrentState
+                            options:UIViewAnimationOptionAllowUserInteraction | UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionLayoutSubviews
                          animations:^{
                              [dismissingView setFrame:dismissingFinalRect];
                              [originalView setFrame:originalFinalRect];
